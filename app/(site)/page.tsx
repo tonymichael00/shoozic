958767d39b90fa44b9fe2f4efc4533357476d803
tonymichael00 <tonymichael00@gmail.com>
2023-12-07 15:57:11 -0600

    added components: Box, Sidebar, SidebarItem

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <main>
       <div className="">
-        TOOOOOOOOOOOOOOOOOOOOGGLE DARK MODE. Is this easy to read? You should be
-        the judge bucko.
+        MAIN CONTENT TOOOOOOOOOOOOOOOOOOOOGGLE DARK MODE. Is this easy to read?
+        You should be the judge bucko.
         <ModeToggle />
       </div>
     </main>
