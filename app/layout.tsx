20a816faa61d9da0239a06adc285329b186303f4
tonymichael00 <tonymichael00@gmail.com>
2023-12-07 13:55:38 -0600

    edited css and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from 'next';
-import { Manrope } from 'next/font/google';
-import './globals.css';
-import { ThemeProvider } from '@/components/theme-provider';
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const manrope = Manrope({ subsets: ['latin'] });
+const manrope = Manrope({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'shoozic',
-  description: 'Better than Spotify',
+  title: "shoozic",
+  description: "Better than Spotify",
 };
 
 export default function RootLayout({
@@ -17,11 +17,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={manrope.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-        >
+      <body className={`${manrope.className} font-medium`}>
+        <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
         </ThemeProvider>
       </body>
