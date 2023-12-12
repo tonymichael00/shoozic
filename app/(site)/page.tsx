f45573b61f0bdcd75feb142da2d6bce86e329574
tonymichael00 <tonymichael00@gmail.com>
2023-12-12 16:50:29 -0600

    fixing tailwind intellisense

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 import { ModeToggle } from "@/components/ui/toggle-mode";
 
 export default function Home() {
@@ -8,7 +9,21 @@ export default function Home() {
         className="h-full w-full overflow-hidden 
       overflow-y-auto rounded-lg bg-neutral-900"
       >
-        <Header>Header</Header>
+        <Header>
+          <div className="mb-2">
+            <h1 className="text-3xl font-semibold text-white">Welcome back</h1>
+            <div
+              className="mt-4 grid grid-cols-1 gap-3 
+            sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4"
+            >
+              <ListItem
+                image="/images/liked.png"
+                name="Liked Songs"
+                href="liked"
+              />
+            </div>
+          </div>
+        </Header>
 
         {/* <ModeToggle /> */}
       </div>
